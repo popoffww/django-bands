b18ed5aae1ffa886f894d3f38d4ee5b992605dcc
popoffww <popoffww@yandex.ru>
2020-09-05 19:51:48 +0600

    First commit

---
+++ group/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from person.models import Person
+
+class Group(models.Model):
+    name = models.CharField(max_length=128)
+    members = models.ManyToManyField(Person, through='Membership')
+
+    def __str__(self):
+        return self.name
---
+++ membership/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from person.models import Person
+from group.models import Group
+
+class Membership(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    date_joined = models.DateField()
+    invite_reason = models.CharField(max_length=64)
