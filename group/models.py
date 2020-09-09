8252bf868a1eb5de7cb440bda1ed6794788d4000
popoffww <popoffww@yandex.ru>
2020-09-09 11:47:33 +0600

    Extends Group

--- group/models.py
+++ group/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from person.models import Person
 
 class Group(models.Model):
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=128, unique=True)
     members = models.ManyToManyField(Person, through='membership.Membership')
 
     def __str__(self):
--- group/views.py
+++ group/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Group
 
-# Create your views here.
+def home(request):
+    groups = Group.objects.all()
+    return render(request, 'group/home.html', {'objects_list': groups})
+
+
