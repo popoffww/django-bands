6fc39b05528c751e4c731f5641815598cfcc6406
popoffww <popoffww@yandex.ru>
2020-09-15 19:56:47 +0600

    Добавил изменение и удаление Membership

--- person/models.py
+++ person/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Person(models.Model):
-    name = models.TextField(max_length=128, unique=True)
+    name = models.TextField(max_length=128, db_index=True)
 
     def __str__(self):
         return self.name
